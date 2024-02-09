38eb8c879a3c7a78ef91783fb2b8edc33ff9cfac
evgeniyshimkov <yakimoto.evgeniy@gmail.com>
2024-02-09 15:41:57 +0200

    add player res

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CardModule } from './card/card.module';
 import { UserModule } from './user/user.module';
 import { CharacterModule } from './character/character.module';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [CardModule, UserModule, CharacterModule],
+  imports: [CardModule, UserModule, CharacterModule, PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
