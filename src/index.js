2f3cdfbfc3490227c77f8ef495e73ef85ebd15c3
dimak911 <dimak911@gmail.com>
2024-02-12 16:00:38 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,24 +3,35 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors())
+
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+  res.send('Works!')
 });
 
 io.on('connection', (socket) => {
+
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
+
+    socket.emit('message', `${new Date().toISOString()}: ${msg}`)
   });
 });
 
 
-server.listen(3000, () => {
+server.listen(3333, () => {
   console.log('server running at http://localhost:3000');
 });
