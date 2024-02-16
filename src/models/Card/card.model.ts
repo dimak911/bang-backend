130aff3fcb18f39d1535c36d4093b964ff1574e7
dimak911 <dimak911@gmail.com>
2024-02-16 09:07:01 +0200

    fix

--- src/models/Card/card.model.ts
+++ src/models/Card/card.model.ts
@@ -7,21 +7,24 @@ import { CardTypeEnum } from '../../common/enums/card/card-type.enum';
 import { CardTargetEnum } from '../../common/enums/card/card-target.enum';
 import { CardActionEnum } from '../../common/enums/card/card-action.enum';
 
-const cardSchema = new Schema<ICard>({
-  title: { type: String, required: true },
-  slug: { type: String, required: true },
-  image: { type: String, required: true },
-  description: { type: String, required: true },
-  suit: { type: String, enum: CardSuitEnum, required: true },
-  rank: { type: String, enum: CardRankEnum, required: true },
-  type: { type: String, enum: CardTypeEnum, required: true },
-  target: { type: String, enum: CardTargetEnum, required: true },
-  cardAction: { type: String, enum: CardActionEnum, required: true },
-  cardBlockers: { type: [Schema.Types.ObjectId], ref: 'Card' },
-  actionAmount: { type: Number, required: true },
-  blockAmount: { type: Number, required: true },
-  distance: { type: Number, required: true },
-});
+const cardSchema = new Schema<ICard>(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    suit: { type: String, enum: CardSuitEnum, required: true },
+    rank: { type: String, enum: CardRankEnum, required: true },
+    type: { type: String, enum: CardTypeEnum, required: true },
+    target: { type: String, enum: CardTargetEnum, required: true },
+    cardAction: { type: String, enum: CardActionEnum, required: true },
+    cardBlockers: { type: [Schema.Types.ObjectId], ref: 'Card' },
+    actionAmount: { type: Number, required: true },
+    blockAmount: { type: Number, required: true },
+    distance: { type: Number, required: true },
+  },
+  { timestamps: true },
+);
 
 const CardModel = model('Card', cardSchema);
 
--- src/models/Character/character.model.ts
+++ src/models/Character/character.model.ts
@@ -2,12 +2,15 @@ import { Schema, model } from 'mongoose';
 
 import { ICharacter } from './character.interface';
 
-const characterSchema = new Schema<ICharacter>({
-  name: { type: String, required: true },
-  health: { type: Number, required: true },
-  image: { type: String, required: true },
-  userName: String,
-});
+const characterSchema = new Schema<ICharacter>(
+  {
+    name: { type: String, required: true },
+    health: { type: Number, required: true },
+    image: { type: String, required: true },
+    userName: String,
+  },
+  { timestamps: true },
+);
 
 const CharacterModel = model('Character', characterSchema);
 
